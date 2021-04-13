8851c0f1a40782b9ebdcd961e7b79794bde95ac6
ShinozukaYuki <chancheep.mcs@gmail.com>
2021-04-13 10:03:43 +0700

    update

---
+++ sec1_gr5_src/JsCategoriespage.js
@@ -0,0 +1,57 @@
+function menu1(){
+    let noticeRef = document.getElementById("notice1");
+    let foodRef = document.getElementById("food");
+    let drinkRef = document.getElementById("drink");
+    let dessertRef = document.getElementById("dessert");
+    let branchRef = document.getElementById("branch");
+    noticeRef.style.display = "none";
+    drinkRef.style.display = "none";
+    dessertRef.style.display = "none";
+    branchRef.style.display = "none";
+    if (foodRef.style.display == "none") {
+        foodRef.style.display = "block";
+    }
+   
+}
+function menu2(){
+    let noticeRef = document.getElementById("notice1");
+    let foodRef = document.getElementById("food");
+    let drinkRef = document.getElementById("drink");
+    let dessertRef = document.getElementById("dessert");
+    let branchRef = document.getElementById("branch");
+    noticeRef.style.display = "none";
+    foodRef.style.display = "none";
+    dessertRef.style.display = "none";
+    branchRef.style.display = "none";
+    if (drinkRef.style.display == "none") {
+        drinkRef.style.display = "block";
+    }
+}
+function menu3(){
+    let noticeRef = document.getElementById("notice1");
+    let foodRef = document.getElementById("food");
+    let drinkRef = document.getElementById("drink");
+    let dessertRef = document.getElementById("dessert");
+    let branchRef = document.getElementById("branch");
+    noticeRef.style.display = "none";
+    drinkRef.style.display = "none";
+    foodRef.style.display = "none";
+    branchRef.style.display = "none";
+    if (dessertRef.style.display == "none") {
+        dessertRef.style.display = "block";
+    }
+}
+function menu4(){
+    let noticeRef = document.getElementById("notice1");
+    let foodRef = document.getElementById("food");
+    let drinkRef = document.getElementById("drink");
+    let dessertRef = document.getElementById("dessert");
+    let branchRef = document.getElementById("branch");
+    noticeRef.style.display = "none";
+    drinkRef.style.display = "none";
+    dessertRef.style.display = "none";
+    foodRef.style.display = "none";
+    if (branchRef.style.display == "none") {
+        branchRef.style.display = "block";
+    }
+}
