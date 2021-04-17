55010be84e7315502d352a8482370e6e272910e7
ShinozukaYuki <chancheep.mcs@gmail.com>
2021-04-17 14:23:15 +0700

    update

---
+++ sec1_gr5_src/JsChecklogin.js
@@ -0,0 +1,17 @@
+function alwayscheck() {
+  const login = localStorage.getItem('login');
+  if(login)
+  {
+    document.getElementById("showLogout").innerHTML = `<button onclick="logoutset()">LOGOUT</button>`;
+  } 
+  else{
+    document.getElementById("showLogin").innerHTML = `<a href="HtmlLoginpage.html">LOGIN</a>`;
+  } 
+}
+
+function logoutset()
+{
+  localStorage.clear();
+  alert("logout successful");
+  location.replace("HtmlLoginpage.html");
+}
